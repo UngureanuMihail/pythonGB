356dcc3a43807797f14c4afc711248b9be2e16ae
Ungureanu Mihail <ungureanu.mmih@gmail.com>
2023-04-04 21:39:14 +0300

    Update function. Add range between 0 and 100000

--- lesson_1/task_3.py
+++ lesson_1/task_3.py
@@ -1,16 +1,18 @@
 # Напишите код, который запрашивает число и сообщает является ли оно простым или составным.
 # Используйте правило для проверки: “Число является простым, если делится нацело только на единицу и на себя”.
 # Сделайте ограничение на ввод отрицательных чисел и чисел больше 100 тысяч.
+from math import sqrt
+
 
 def check_num_type(x):
     v = 2
-    if x % v != 0:  # until the remainder of the division is not 0
-        v += 1  # plus 1 unit to v
-    if v == x:  # in case if v number is equal by checked one this number prime
-        print('This number is prime')
+    if 0 < x <= 100000:
+        while v <= sqrt(x):
+            if x % v == 0:  # until the remainder of the division is  0
+                print("This number is composite")
+                break
+            v += 1  # plus 1 unit to v
+        else:
+            print("This number is prime")
     else:
-        print('This number is composite')  # in other case this is composite number
-
-
-check_num_type(4)  # composite number test
-check_num_type(2)  # prime number test
+        print('Error introduce number between 0 and 100000')
